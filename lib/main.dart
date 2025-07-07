c1e8bb44adf211eb11727ce05fe477c01013533b
carlo10101010 <mendozacarlo185@gmail.com>
2025-07-07 12:26:40 +0800

    World_Time

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:church_reminder/pages/Addreminder.dart';
 import 'package:church_reminder/pages/Dashboard.dart';
+import 'package:church_reminder/pages/Itemcart.dart';
 import 'package:church_reminder/pages/ListItems.dart';
+import 'package:church_reminder/World_Time/Home.dart';
+import 'package:church_reminder/World_Time/Loading.dart';
+import 'package:church_reminder/World_Time/Choose_Location.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+
+void main() {
  runApp(MaterialApp(
- routes: {
-  '/' : (context) => Listitems(),
-  '/add' : (context) => Addreminder()
- }
-));
+  initialRoute: '/home',
+  routes: {
+   '/dashboard': (context) => Dashboard(),
+   '/add' : (context) => Addreminder(),
+   '/loading' : (context) => Loading(),
+   '/' : (context) => Home(),
+   '/location' : (context) => ChooseLocation()
+
+  },
+ ));
 }
